7297775f75149c3b13601ba6bb542305e44576ff
Lori Louie <lorilouie@llouie-2K7D.local>
2026-02-04 12:15:47 -0800

    setup github pages deployment

--- AI-Filtering/vite.config.ts
+++ AI-Filtering/vite.config.ts
@@ -5,4 +5,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+  base: '/AI-filtering/', // Replace with your exact repo name
 })
